a8ec98bb987d3b42c4b70f1b989b696ee4c01a6a
Maima Ismail <maimamirza@gmail.com>
2023-09-28 23:58:58 +0500

    services commit

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -1,11 +1,12 @@
 import ServiceHeader from './serviceHeader/serviceHeader'
 import ServiceSlides from './serviceSlides/serviceSlides'
+import './index.css'
 const Services = () => {
   return (
     <div className="services">
       <ServiceHeader />
       <div className="services-container">
-        <div className="hidden-div">nothing</div>
+        <div className="hidden-div"></div>
         <ServiceSlides />
       </div>
     </div>
