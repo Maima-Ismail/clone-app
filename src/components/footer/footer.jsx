69af46653477c55efa522fd44feb409ab432e853
Maima Ismail <maimamirza@gmail.com>
2023-09-30 22:20:12 +0500

    Email validation done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,15 +3,24 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import arrow from '../../assets/arrow-right-white.png'
+import { useFormik } from 'formik'
 import './footer.css'
+import { NewsletterSchema } from '../../schemas'
 const Footer = () => {
+  const initialValues = {
+    email: '',
+  }
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    validationSchema: NewsletterSchema,
+    onSubmit: (value, action) => {
+      console.log(value)
+      action.resetForm()
+    },
+  })
   return (
     <div className="footer-container">
-      <div className="footer-icons-top">
-        <FontAwesomeIcon icon={faLinkedinIn} id="icon1" className="icon" />
-        <FontAwesomeIcon icon={faFacebookF} id="icon2" className="icon" />
-        <FontAwesomeIcon icon={faInstagram} id="icon3" className="icon" />
-      </div>
+      <Icons className="footer-icons-top" />
       <div className="footer-first-div">
         <div className="footer-about">
           <p className="h1">About us</p>
@@ -31,22 +40,37 @@ const Footer = () => {
           Join our news letter to Know our latest feeds
         </p>
         <div className="newsletter-input">
-          <input type="text" placeholder="Email Address" />
-          <button type="submit">
-            <img src={arrow} alt="Enter" />
-          </button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Email Address"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            <button type="submit">
+              <img src={arrow} alt="Enter" />
+            </button>
+            {errors.email ? <p className="form-error">{errors.email}</p> : null}
+          </form>
         </div>
       </div>
       <div className="footer-tos">
         <p className="h2">Terms of Services</p>
         <p className="h2">Privacy Policy</p>
       </div>
-      <div className="footer-icons">
-        <FontAwesomeIcon icon={faLinkedinIn} id="icon1" className="icon" />
-        <FontAwesomeIcon icon={faFacebookF} id="icon2" className="icon" />
-        <FontAwesomeIcon icon={faInstagram} id="icon3" className="icon" />
-      </div>
+      <Icons className="footer-icons" />
     </div>
   )
 }
 export default Footer
+
+export const Icons = ({ className }) => {
+  return (
+    <div className={className}>
+      <FontAwesomeIcon icon={faLinkedinIn} id="icon1" className="icon" />
+      <FontAwesomeIcon icon={faFacebookF} id="icon2" className="icon" />
+      <FontAwesomeIcon icon={faInstagram} id="icon3" className="icon" />
+    </div>
+  )
+}
