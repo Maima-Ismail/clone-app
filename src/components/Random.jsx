41819eebb8a90c8bc7c18747680e044a173bc18b
Maima Ismail <maimamirza@gmail.com>
2023-10-04 19:03:59 +0500

    used useRef

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,10 +1,15 @@
+import { useRef } from 'react'
 const Random = () => {
+  const inputElement = useRef(null)
+  const inputElement2 = useRef(null)
   const variable = {
     email: '',
     password: '',
   }
   const handleSubmit = (e) => {
     e.preventDefault()
+    variable.email = inputElement.current.value
+    variable.password = inputElement2.current.value
     console.log(variable)
   }
   return (
@@ -14,19 +19,13 @@ const Random = () => {
           type="email"
           name="email"
           placeholder="Email Address"
-          defaultValue={variable.email}
-          onChange={(e) => {
-            variable.email = e.target.value
-          }}
+          ref={inputElement}
         />
         <input
           type="password"
           name="password"
           placeholder="Password"
-          defaultValue={variable.password}
-          onChange={(e) => {
-            variable.password = e.target.value
-          }}
+          ref={inputElement2}
         />
         <button type="submit">Submit</button>
       </form>
