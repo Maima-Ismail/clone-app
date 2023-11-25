370075fb326ba7a334a593c84763c8b771709673
Maima-Ismail <maimamirza@gmail.com>
2023-11-25 22:10:42 +0500

    home-page

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,12 +2,14 @@ import HomePageSlider from './HomePageSlider/HomePageSlider'
 import HomePageProjects from './HomePageProjects/HomePageProjects'
 import HomePageServices from './HomePageServices/HomePageServices'
 import Footer from '../footer/footer'
+import Goal from './Goal.jsx/Goal'
 const HomePage = () => {
   return (
     <>
       <HomePageSlider />
       <HomePageProjects />
       <HomePageServices />
+      <Goal />
       <Footer />
     </>
   )
