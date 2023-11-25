76b0dd1458e7c9be8883f3c0b0c9917ff2875c6d
Maima-Ismail <maimamirza@gmail.com>
2023-11-25 18:42:03 +0500

    services done

--- src/components/HomePage/HomePageServices/HomePageServices.jsx
+++ src/components/HomePage/HomePageServices/HomePageServices.jsx
@@ -1,13 +1,16 @@
 import './HomePageServices.css'
-import Image1 from '../../../assets/ServicesWeWork1.png'
-import Image2 from '../../../assets/ServicesWeWork2.png'
+import Image1 from '../../../assets/ServicesWeWork2.png'
+import Image2 from '../../../assets/ServicesWeWork1.png'
 const HomePageServices = () => {
   return (
     <div className="home-services-container">
       <p className="home-services-heading1">Our Services</p>
       <p className="home-services-heading2">Services We Work Towards</p>
-      <div className="home-service1">
-        <p className="home-service1-heading">Software Engineering</p>
+      <div className='services-portion'>
+      <div className='services-first-div'>
+      <div className="home-service" id='home-service1'>
+        <p className="home-service-heading">Software Engineering</p>
+        <div className='home-service-lists1'>
         <p className="home-service-list" id="service1-list1">
           Redesign
         </p>
@@ -20,9 +23,12 @@ const HomePageServices = () => {
         <p className="home-service-list" id="service1-list4">
           Software testing
         </p>
+        </div>
+        <img src={Image1} alt="Software Engineering" />
       </div>
-      <div className="home-service2">
-        <p className="home-service2-heading">Product Development</p>
+      <div className="home-service" id='home-service2'>
+        <p className="home-service-heading">Product Development</p>
+        <div className='home-service-lists2'>
         <p className="home-service-list" id="service2-list1">
           SaaS Development
         </p>
@@ -32,9 +38,15 @@ const HomePageServices = () => {
         <p className="home-service-list" id="service2-list3">
           Entire business in one ecosystem
         </p>
+        </div>
+        <img src={Image2} alt="Product Development" />
       </div>
-      <div className="home-service3">
-        <p className="home-service3-heading">Web Portal Development</p>
+      </div>
+
+    <div className='services-second-div'>
+      <div className="home-service" id='home-service3'>
+        <p className="home-service-heading">Web Portal Development</p>
+        <div className='home-service-lists3'>
         <p className="home-service-list" id="service3-list1">
           Employee/Customer portals
         </p>
@@ -47,9 +59,11 @@ const HomePageServices = () => {
         <p className="home-service-list" id="service3-list4">
           eCommerce portals
         </p>
+        </div>
       </div>
-      <div className="home-service4">
-        <p className="home-service4-heading">Managed IT Service</p>
+      <div className="home-service" id='home-service4'>
+        <p className="home-service-heading">Managed IT Service</p>
+        <div className='home-service-lists4'>
         <p className="home-service-list" id="service4-list1">
           Outsourced product development
         </p>
@@ -62,6 +76,9 @@ const HomePageServices = () => {
         <p className="home-service-list" id="service4-list4">
           Dev teams
         </p>
+        </div>
+    </div>
+      </div>
       </div>
     </div>
   )
