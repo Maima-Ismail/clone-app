f9ea2d3cacd0caa66e699540294ccde0f621b5c2
Maima-Ismail <maimamirza@gmail.com>
2023-12-03 19:28:05 +0500

    industries section complete

--- src/components/HomePage/HomeIndustriesSection/HomeIndustriesSection.jsx
+++ src/components/HomePage/HomeIndustriesSection/HomeIndustriesSection.jsx
@@ -1,51 +1,78 @@
-import { useState, useEffect } from 'react'
-import './HomeIndustriesSection.css'
-import industries from '../../../Data Files/industries'
+import { useState, useEffect, useRef } from 'react';
+import './HomeIndustriesSection.css';
+import industries from '../../../Data Files/industries';
+
 const HomeIndustriesSection = () => {
-    const [currentIndustry, setCurrentIndustry] = useState(0)
+    const [currentIndustry, setCurrentIndustry] = useState(0);
+    const [direction, setDirection] = useState(1); // 1 for left-to-right, -1 for right-to-left
+    const industryNameRef = useRef(null);
+    const industryBarRef = useRef(null);
+
     const handleClick = (index) => {
-        setCurrentIndustry(index)
-    }
+        const newDirection = index > currentIndustry ? 1 : -1;
+        setDirection(newDirection);
+        setCurrentIndustry(index);
+    };
+
     useEffect(() => {
-        const interval = setInterval(()=>{
-            setCurrentIndustry( (prevIndustry) => (prevIndustry + 1) % industries.length)
-        }, 5000)
+        const interval = setInterval(() => {
+            const newDirection = (direction === 1 && currentIndustry === industries.length - 1) ? -1 : direction;
+            setCurrentIndustry((prevIndustry) => (prevIndustry + newDirection) % industries.length);
+            setDirection(newDirection);
+        }, 5000);
+
         return () => {
-            clearInterval(interval)
+            clearInterval(interval);
+        };
+    }, [currentIndustry, direction]);
+
+    useEffect(() => {
+        const activeIndustryName = industryNameRef.current.querySelector('.active-industry-name');
+        const industryNameBar = industryBarRef.current;
+
+        if (activeIndustryName && industryNameBar) {
+            const { width, left } = activeIndustryName.getBoundingClientRect()
+            industryNameBar.style.width = `${width}px`;
+            industryNameBar.style.left = `${left}px`;
         }
-    }, [currentIndustry])
-    return(
+    }, [currentIndustry]);
+
+    return (
         <div className='home-industries-section'>
             <p className='industries-heading'>Industries we worked in</p>
-            <div className='industries-container'>
+            <div className='industries-container' ref={industryNameRef}>
                 <div className='industry-name-container'>
-            {industries.map((industry,index)=>(
-                <div className={`industry-name ${currentIndustry === index ? 'active-industry-name' : ''}`}
-                key={index}
-                onClick={()=>{
-                    handleClick(index)
-                }}
-                >
-                    {industry.name}
+                    {industries.map((industry, index) => (
+                        <div
+                            className={`industry-name ${currentIndustry === index ? 'active-industry-name' : ''}`}
+                            key={index}
+                            onClick={() => {
+                                handleClick(index);
+                            }}
+                        >
+                            {industry.name}
+                        </div>
+                    ))}
                 </div>
-            ))}
-            </div>
-            <div className='industry-slide-container'>
-            {industries.map((industry,index)=>(
-                <div className={`industry-slide ${currentIndustry === index ? 'active-industry' : ''}`}
-                key={index}
-                >
-                    <img src={industry.image.src} alt={industry.image.alt} className='industry-image'/>
-                    <div className='industry-description-bg'>
-                        <p className='industry-description'>{industry.description}</p>
-                    </div>
+                <div className="industry-name-bar" ref={industryBarRef}></div>
+                <div className='break-industries-section'></div>
+                <div className='industry-slide-container'>
+                    {industries.map((industry, index) => (
+                        <div
+                            className={`industry-slide ${currentIndustry === index ? 'active-industry' : ''}`}
+                            key={index}
+                        >
+                            <img src={industry.image.src} alt={industry.image.alt} className='industry-image' />
+                            <div className='industry-description-bg'>
+                                <p className='industry-description'>{industry.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                    <div className='industries-white-div'></div>
                 </div>
-            ))}
-            <div className='industries-white-div'></div>
-            </div>
             </div>
         </div>
+    );
+};
 
-    )
-}
-export default HomeIndustriesSection
+export default HomeIndustriesSection;
