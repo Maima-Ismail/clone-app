715d12dd2b4d905427e4e12e7c8793b9daeee299
Maima-Ismail <maimamirza@gmail.com>
2024-01-02 00:21:36 +0500

    terms and conditions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import './App.css'
-import Home from './pages/Home/index'
-import Projects from './pages/Projects/index'
-import ContactUs from './pages/ContactUs/index'
-import Services from './pages/Services'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./App.css";
+import Home from "./pages/Home/index";
+import Projects from "./pages/Projects/index";
+import ContactUs from "./pages/ContactUs/index";
+import Services from "./pages/Services";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TermsServices from "./pages/Terms and Services";
+import PrivacyPolicy from "./pages/Privacy Policy";
 const router = createBrowserRouter([
-  { path: '/', element: <Home/> },
+  { path: "/", element: <Home /> },
   {
-    path: '/contact',
+    path: "/contact",
     element: <ContactUs />,
   },
   {
-    path: '/services',
+    path: "/services",
     element: <Services />,
   },
   {
-    path: '/projects',
-    element: <Projects/>
+    path: "/projects",
+    element: <Projects />,
   },
-])
+  {
+    path: "/terms-and-conditions",
+    element: <TermsServices />,
+  },
+  {
+    path: "privacy-policy",
+    element: <PrivacyPolicy />,
+  },
+]);
 function App() {
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
